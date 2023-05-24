ee6b17b1bfdf3fc3595d22223caa2f1242e08b62
yhcho0405 <yhcho0405@kakao.com>
2023-05-24 13:07:37 +0900

    facial detect

--- main.py
+++ main.py
@@ -2,54 +2,79 @@ import cv2
 import dlib
 import numpy as np
 
-# Initialize Dlib's face detector and facial landmark predictor
+# Initialize dlib's face detector and the facial landmarks predictor
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-# Capture video from webcam
+# Start the webcam with OpenCV
 cap = cv2.VideoCapture(0)
 
-def annotate_landmarks(im, landmarks):
-    im = im.copy()
-    for idx, point in enumerate(landmarks):
-        pos = (point[0, 0], point[0, 1])
-        cv2.putText(im, str(idx), pos,
-                    fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
-                    fontScale=0.4,
-                    
-                    color=(0, 0, 255))
-        cv2.circle(im, pos, 3, color=(0, 255, 255))
-    return im
+if not cap.isOpened():
+    print("Error opening video stream")
 
 while True:
+    # Capture frame-by-frame
     ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Get the size of the frame
+    height, width = frame.shape[:2]
+
+    # Convert color to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect faces
     faces = detector(gray)
 
-    image_with_landmarks = gray
-
+    # Iterate over the faces detected
     for face in faces:
-        x, y = face.left(), face.top()
-        x1, y1 = face.right(), face.bottom()
-        cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
+        # Get the landmarks/parts for the face
+        shape = predictor(gray, face)
 
-        # Get facial landmarks
-        landmarks = predictor(gray, face)
-        image_with_landmarks = annotate_landmarks(frame, landmarks)
+        # Get coordinates for the centers of the eyes
+        right_eye_center = np.mean(np.array([(shape.part(i).x, shape.part(i).y) for i in range(36, 42)]), axis=0).astype(int)
+        left_eye_center = np.mean(np.array([(shape.part(i).x, shape.part(i).y) for i in range(42, 48)]), axis=0).astype(int)
 
-        # Draw circles for the eye centers
-        for i in range(36, 48):
-            x = landmarks.part(i).x
-            y = landmarks.part(i).y
-            cv2.circle(frame, (x, y), 2, (255, 0, 0), -1)
+        # Calculate the center point between the two eyes
+        center_of_eyes = np.mean(np.array([right_eye_center, left_eye_center]), axis=0).astype(int)
 
-    cv2.imshow("Frame", image_with_landmarks)
+        # Print the coordinates
+        print(f"Right eye center: {right_eye_center}")
+        print(f"Left eye center: {left_eye_center}")
+        print(f"Center of eyes: {center_of_eyes}")
 
+        # Draw small circles at each of the 68 facial landmarks
+        for i in range(68):
+            cv2.circle(frame, (shape.part(i).x, shape.part(i).y), 1, (0, 255, 0), thickness=2)
+
+        # Shift frame around the center of eyes
+        eye_x, eye_y = center_of_eyes
+        shift_x = width // 2 - eye_x
+        shift_y = height // 2 - eye_y
+
+        # Define transformation matrix for shift
+        M_shift = np.float32([[1, 0, shift_x], [0, 1, shift_y]])
+        shifted_frame = cv2.warpAffine(frame, M_shift, (width, height))
+
+        # Compute the angle between the line connecting the centers of the eyes and the horizontal line
+        delta_x = right_eye_center[0] - left_eye_center[0]
+        delta_y = right_eye_center[1] - left_eye_center[1]
+        angle = np.degrees(np.arctan2(delta_y, delta_x))
+
+        # Rotate frame to make the line between the eyes horizontal
+        M_rotate = cv2.getRotationMatrix2D((width // 2, height // 2), angle - 180, 1)
+        rotated_frame = cv2.warpAffine(shifted_frame, M_rotate, (width, height))
+
+        frame = rotated_frame
+
+    # Display the resulting frame with landmarks
+    cv2.imshow('Webcam Feed', frame)
+    # Break the loop on 'q' press
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# When everything done, release the capture and destroy the windows
 cap.release()
 cv2.destroyAllWindows()
-
